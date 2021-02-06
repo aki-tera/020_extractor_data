a5a5562a78014983c9fb1faf5f6b045af0bef53d
aki-tera <aki.tera@gmail.com>
2021-02-06 14:05:13 +0900

    Ver1.6 feat:use setting values

--- 020_extractor_data.py
+++ 020_extractor_data.py
@@ -10,21 +10,38 @@ from matplotlib.pyplot import figure
 
 
 class DataExtractor:
-    def __init__(self, filename):
+    def __init__(self, filename, dict):
         # ファイル名
-        self.filename = filename
+        self.DEFilename = filename
+        # 各種設定を定義
+        self.DECol1 = list(dict.keys())[0]
+        self.DECol2 = list(dict.keys())[1]
+        self.DECol3 = list(dict.keys())[2]
+
+        self.DEHigh1 = dict[self.DECol1]["high"]
+        self.DELow1 = dict[self.DECol1]["low"]
+
+        self.DEHigh2 = dict[self.DECol2]["high"]
+        self.DELow2 = dict[self.DECol2]["low"]
+
+        self.DEFirst = dict[self.DECol3]["first"]
 
 
     def create_dataframe(self):
         # ファイルの読み出し
-        self.df = pd.read_csv(self.filename, encoding="SHIFT-JIS", engine='python')
+        self.df = pd.read_csv(self.DEFilename, encoding="SHIFT-JIS", engine='python')
         # 差分を追加
-        #self.df = self.df.assign(diff=self.df["OUT1"] - self.df["OUT2"])
-        self.df["diff"] = self.df["OUT1"] - self.df["OUT2"]
+        if self.DEFirst == self.DECol1:
+            self.df[self.DECol3] = self.df[self.DECol1] - self.df[self.DECol2]
+        else:
+            self.df[self.DECol3] = self.df[self.DECol2] - self.df[self.DECol1]
 
-    def separate_dataframe(self, SD_low, SD_high):
+    def separate_dataframe(self):
         # 該当のINDEXを抽出
-        list = self.df.query("({0} < OUT1 | {0} < OUT2) & (OUT1 < {1} | OUT2 < {1})".format(SD_low,SD_high)).index
+        #list = self.df.query("({0} < OUT1 | {0} < OUT2) & (OUT1 < {1} | OUT2 < {1})".format(SD_low,SD_high)).index
+        query_eva1 = "({1} < {0} & {0} < {2})".format(self.DECol1,self.DELow1,self.DEHigh1)
+        query_eva2 = "({1} < {0} & {0} < {2})".format(self.DECol2,self.DELow2,self.DEHigh2)
+        list = self.df.query(query_eva1+" | "+query_eva2).index
         # INDEXの塊をリストに切り分け
         self.result = []
         val_pre = 0
@@ -39,29 +56,29 @@ class DataExtractor:
             val_pre = val
         # 各列ごとにファイルにまとめる
         # 各列ごとのデータフレームの初期化
-        self.df1 = pd.DataFrame(index=range(1000))
-        self.df2 = pd.DataFrame(index=range(1000))
-        self.df3 = pd.DataFrame(index=range(1000))
+        self.df1 = pd.DataFrame(index=range(100))
+        self.df2 = pd.DataFrame(index=range(100))
+        self.df3 = pd.DataFrame(index=range(100))
         # 各データフレームに値を入れていく
         for i, temp in enumerate(self.result):
             label = "{0:03}".format(i)
-            self.df1["1" + label] = pd.DataFrame(self.df[temp[0]:temp[-1]]["OUT1"]
-                                    .reset_index().loc[:, ["OUT1"]])
-            self.df2["2" + label] = pd.DataFrame(self.df[temp[0]:temp[-1]]["OUT2"]
-                                    .reset_index().loc[:, ["OUT2"]])
-            self.df3["3" + label] = pd.DataFrame(self.df[temp[0]:temp[-1]]["diff"]
-                                    .reset_index().loc[:, ["diff"]])
+            self.df1["1" + label] = pd.DataFrame(self.df[temp[0]:temp[-1]][self.DECol1]
+                                    .reset_index().loc[:, [self.DECol1]])
+            self.df2["2" + label] = pd.DataFrame(self.df[temp[0]:temp[-1]][self.DECol2]
+                                    .reset_index().loc[:, [self.DECol2]])
+            self.df3["3" + label] = pd.DataFrame(self.df[temp[0]:temp[-1]][self.DECol3]
+                                    .reset_index().loc[:, [self.DECol3]])
 
     def save_dataframe(self):
         # ファイルに書き込み
         # excelで開きたいのでshift-jisを指定する
-        self.df1.to_csv(self.filename[:-4] + "-out1.csv", encoding="shift_jis")
-        self.df2.to_csv(self.filename[:-4] + "-out2.csv", encoding="shift_jis")
-        self.df3.to_csv(self.filename[:-4] + "-diff.csv", encoding="shift_jis")
+        self.df1.to_csv(self.DEFilename[:-4] + "-out1.csv", encoding="shift_jis")
+        self.df2.to_csv(self.DEFilename[:-4] + "-out2.csv", encoding="shift_jis")
+        self.df3.to_csv(self.DEFilename[:-4] + "-diff.csv", encoding="shift_jis")
 
     def plot_dataframe(self):
         # 参考までに表示
-        fig = figure(figsize=(20, 10))
+        fig = figure(figsize=(10, 8))
         for i in range(9):
             num = i
 
@@ -70,26 +87,23 @@ class DataExtractor:
             # 折れ線グラフ
             self.df[self.result[num][0]:self.result[num][-1]].plot(ax=ax)
             # 縦軸の値を指定
-            ax.set_ylim(bottom=-0.1, top=0.1)
+            ax.set_ylim(bottom=-5, top=5)
             # グリッド表示
             ax.grid(True)
 
 
 def main():
     # パラメータの取り出し
-    setting = open("setting.json", "r")
+    setting = open("setting.json", "r", encoding="utf-8")
     dict = json.load(setting)
-    # 辞書から取り出したパラメータをセットする
-    high = dict["high"]
-    low = dict["low"]
 
     # ファイル読み込み
     filename = glob.glob("*.csv")
     for row in filename:
         if row[-9:] not in ["-out1.csv", "-out2.csv", "-diff.csv"]:
-            d = DataExtractor(row)
+            d = DataExtractor(row, dict)
             d.create_dataframe()
-            d.separate_dataframe(low, high)
+            d.separate_dataframe()
             d.save_dataframe()
             d.plot_dataframe()
     plt.show()
